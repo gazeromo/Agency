085d9d55aa99400541949974a6db525118a4cb66
Vladislav <gazeromo@gmail.com>
2018-12-17 18:44:13 +0300

    add clients form

--- Agency/ClientWindow.xaml.cs
+++ Agency/ClientWindow.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Agency
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ClientWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ClientWindow : Window
     {
-        public MainWindow()
+        public ClientWindow()
         {
             InitializeComponent();
-        }
-
-        private void Clients_Click(object sender, RoutedEventArgs e)
-        {
-            Client
-        }
-
-        private void Agents_Click(object sender, RoutedEventArgs e)
-        {
-            AgentWindow window = new AgentWindow();
-            window.Show();
         }
     }
 }
